95a6fba6ac921d60d5c97bfe674205aba871ae11
Samuel Soares <samuelsoares179@gmail.com>
2017-08-19 10:47:53 -0300

    Comunicação com o banco ok

--- Model/DataModule.dfm
+++ Model/DataModule.dfm
@@ -1,5 +1,19 @@
 object DataModule1: TDataModule1
   OldCreateOrder = False
-  Height = 281
-  Width = 521
+  Height = 308
+  Width = 502
+  object FDConnection1: TFDConnection
+    Params.Strings = (
+      'Port=3050'
+      'ConnectionDef=ConexaoCRUD')
+    Left = 96
+    Top = 112
+  end
+  object FDPhysFBDriverLink1: TFDPhysFBDriverLink
+    VendorLib = 
+      'C:\Users\samue\Documents\Embarcadero\Studio\Projects\ProjetoBD\f' +
+      'bclient.dll'
+    Left = 232
+    Top = 136
+  end
 end
--- Model/DataModule.pas
+++ Model/DataModule.pas
@@ -3,10 +3,16 @@ unit DataModule;
 interface
 
 uses
-  System.SysUtils, System.Classes;
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.FB,
+  FireDAC.Phys.FBDef, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client,
+  FireDAC.Phys.IBBase;
 
 type
   TDataModule1 = class(TDataModule)
+    FDConnection1: TFDConnection;
+    FDPhysFBDriverLink1: TFDPhysFBDriverLink;
   private
     { Private declarations }
   public
