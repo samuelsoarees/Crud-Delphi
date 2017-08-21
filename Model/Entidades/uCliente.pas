31167a017ac905bff0b21354f8cec6c69c5af3d9
Samuel Soares <samuelsoares179@gmail.com>
2017-08-21 10:48:58 -0300

    Adicionando entidade cliente ao projeto

---
+++ Model/Entidades/uCliente.pas
@@ -0,0 +1,41 @@
+unit uCliente;
+
+interface
+
+type
+TCliente = class
+  private
+    Fnome:  string;
+    Fcpf :  string;
+    Femail: string;
+    Fsenha: string;
+  
+  public
+    property nome :  string read Fnome  write Fnome;
+    property cpf :   string read Fcpf   write Fcpf;
+    property email : string read Femail write Femail;
+    property senha : string read Fsenha write Fsenha;
+    constructor create(nome:string; cpf:string; email:string; senha:string);
+    constructor createEmpty();
+end;
+
+
+implementation
+
+
+{ Cliente }
+
+constructor TCliente.create(nome:string; cpf:string; email:string; senha:string);
+begin
+  self.Fnome:= nome;
+  self.Fcpf := cpf;
+  self.Femail:=email;
+  self.Fsenha:=senha;
+end;
+
+constructor TCliente.createEmpty;
+begin
+
+end;
+
+end.
